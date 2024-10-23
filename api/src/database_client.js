5c988f009daa443c2f18a06bb430de09c9dd4f14
FurkannOzbek <furkannozbek@yandex.com>
2024-10-23 19:55:44 +0200

    v2

--- api/src/database_client.js
+++ api/src/database_client.js
@@ -1,16 +1,16 @@
 import knex from "knex";
-//import dotenv from "dotenv";
+import dotenv from "dotenv";
 
-//dotenv.config();
+dotenv.config();
 
 const connection = knex({
   client: "pg",
   connection: {
-    host: "dpg-cs2kh3d6l47c73bk9640-a",
-    port: 5432,
-    user: "mealsharing_d3ug_user",
-    password: "8JWqUaQ9ltWeL5cHKSgAHCgeC8WoqjJU",
-    database: "mealsharing_d3ug",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   },
 });
 
